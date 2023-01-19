4740f2131d798c5c1426121a5a6916ed67c9fa0f
NSKZ19997 <nskz19997@gmail.com>
2023-01-19 18:26:58 +0300

    .

--- test_case/constants.py
+++
@@ -1,5 +0,0 @@
-# Файл содержит все константы для проекта (их немного)
-
-seed = 123  # Сид (для возможности воспроизвести результаты)
-categorical_columns = ['feature_3', 'feature_4', 'feature_8', 'feature_9', 'feature_10']  # Все категориальные столбцы датасета
-scalar_columns = ['feature_0', 'feature_1', 'feature_2', 'feature_5', 'feature_6', 'feature_7']  # Все скалярные столбцы датасета
